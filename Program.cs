e5429682bdbb2daf15ea826ca1eb28520172034e
AlexLopez75 <alex.lopez.7e9@itb.cat>
2025-11-11 13:04:22 +0100

    Added random number generation

--- Program.cs
+++ Program.cs
@@ -3,56 +3,24 @@ public class Program
 {
     public static void Main()
     {
-        const string Start = "Enter 10 numbers between 1 and 9";
-        const string InputNumber = "Input value {0}:";
-        const string InputError = "Enter a number between 1 and 9.";
+        const string NumberList = "There are 10 numbers between 1 and 9 generated at random";
         const string NormalOrder = "\nNormal order: ";
         const string InverseOrder = "\nInverse order: ";
-        const int MinNumber = 1;
-        const int MaxNumber = 9;
 
-        int i = 0, number, input = 1;
+        int i = 0, input = 1;
         int[] arrayNums = new int[10];
-        bool isValid;
 
-        Console.WriteLine(Start);
+        Random rand = new Random();
+
+        Console.WriteLine(NumberList);
 
         for (input = 0; input < 10; input++)
         {
-            Console.WriteLine(InputNumber, input + 1);
-            try
-            {
-                isValid = Int32.TryParse(Console.ReadLine(), out number);
-                if (isValid)
-                {
-                    if (number >= MinNumber && number <= MaxNumber)
-                    {
-                        arrayNums[i] = number;
-                        i++;
-                    }
-                    else
-                    {
-                        Console.WriteLine(InputError);
-                        input--;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(InputError);
-                    input--;
-                }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine(InputError);
-                input--;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine(InputError);
-                input--;
-            }
+            int rnd = rand.Next(1, 10);
+            arrayNums[i] = rnd;
+            i++;
         }
+
         Console.Write(NormalOrder);
         foreach (int j in arrayNums)
         {
