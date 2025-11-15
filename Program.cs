fabd299eb92e8cb01b79739dd43925314edc61a9
AlexLopez75 <alex.lopez.7e9@itb.cat>
2025-11-15 12:44:08 +0100

    Changed input variable for local i variable

--- Program.cs
+++ Program.cs
@@ -8,50 +8,48 @@ public class Program
         const string EvenNumbers = "\nEven numbers: ";
         const string OddNumbers = "\nOdd numbers: ";
 
-        int i=0, number, input = 1;
+        int number;
         int[] arrayNumsEven = new int[20];
         int[] arrayNumsOdd = new int[20];
 
-        for (input = 0; input < 20; input++)
+        for (int i = 0; i < 20; i++)
         {
-            Console.WriteLine(InputNumber, input + 1);
+            Console.WriteLine(InputNumber, i + 1);
             try
             {
                 number = Int32.Parse(Console.ReadLine());
                 if (number % 2 == 0)
                 {
                     arrayNumsEven[i] = number;
-                    i++;
                 }
                 else
                 {
                     arrayNumsOdd[i] = number;
-                    i++;
                 }
             }
             catch (FormatException)
             {
                 Console.WriteLine(InputError);
-                input--;
+                i--;
             }
             catch (Exception)
             {
                 Console.WriteLine(InputError);
-                input--;
+                i--;
             }
         }
-        i = 0;
+
         Console.Write(EvenNumbers);
-        for (i = 0; i < arrayNumsEven.Length; i++)
+        for (int i = 0; i < arrayNumsEven.Length; i++)
         {
             if (arrayNumsEven[i] != 0)
             {
                 Console.Write($"{arrayNumsEven[i]} ");
             }
         }
-        i = 0;
+
         Console.Write(OddNumbers);
-        for (i = 0; i < arrayNumsOdd.Length; i++)
+        for (int i = 0; i < arrayNumsOdd.Length; i++)
         {
             if (arrayNumsOdd[i] != 0)
             {
